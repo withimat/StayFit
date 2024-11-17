731c737fdbbdfefdcba9e5987dd36dcbaa9c81f9
İmat Gökaslan <imattgokk@gmail.com>
2024-11-17 13:43:56 +0300

    guncelleme

---
+++ StayFit/ViewModels/Antrenor/WorkoutPlanViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  WorkoutPlanViewModel.swift
+//  StayFit
+//
+//  Created by İmat Gökaslan on 18.11.2024.
+//
+
+import Foundation
---
+++ StayFit/Views/Antrenor/AntrenorWorkoutPlanView.swift
@@ -0,0 +1,18 @@
+//
+//  AntrenorWorkoutPlanView.swift
+//  StayFit
+//
+//  Created by İmat Gökaslan on 19.11.2024.
+//
+
+import SwiftUI
+
+struct AntrenorWorkoutPlanView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AntrenorWorkoutPlanView()
+}
